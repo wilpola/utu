4d6e237263478b2ea2889a40e6e043e4bed3cf65
wilpola <wilpolaproduction@gmail.com>
2023-12-22 10:57:34 +0200

    Create simple routing structure

---
+++ src/pages/About/index.tsx
@@ -0,0 +1,15 @@
+// Code for the about page
+
+import { NavLink as Link } from "react-router-dom";
+
+const About = () => {
+  return (
+    <>
+        <Link end to={'/'}>Home page</Link>
+      <h1>About page</h1>
+    </>
+  );
+};
+
+// export
+export default About;
